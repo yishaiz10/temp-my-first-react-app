1be36e8bd2ce3f6c35bce88c9b54de71c34e9d62
yishai <yishai5555@gmail.com>
2019-08-25 11:39:22 +0300

    b4-workin-class-base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,79 @@
-// import React, { Component } from "react";
-import React, { useState } from "react";
+import React, { Component } from "react";
+// import React, { useState } from "react";
 
 import "./App.css";
 import Person from "./Person/Person";
 
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Yishai", age: 30 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log("switch name was clicked");
+
+    const persons = this.state.persons;
+
+    persons[0].name = "react";
+
+    console.table(persons);
+
+    this.setState({ persons: persons });
+  };
+
+  addPersonHandler = () => {
+    console.log("addPerson was clicked");
+
+    const persons = this.state.persons;
+
+    persons.push({ name: "new person", age: 100 });
+
+    console.table(persons);
+
+    this.setState({ persons: persons });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My first react app</h1>
+
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.addPersonHandler}>Add Person</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <hr />
+        <h2> persons list</h2>
+        {this.state.persons.map(person => {
+          return (
+            <Person
+              name={person.name}
+              age={person.age}
+              // key={person.name}
+            />
+          );
+        })}
+
+        <hr />
+
+        <Person name="aaa" booksCount={4} bookCost={30} />
+        <Person name="person with children" booksCount={4} bookCost={30}>
+          i am a children !!!
+        </Person>
+      </div>
+    );
+  }
+}
+export default App;
+
+/*
 const app = props => {
   const [personsState, setPersonsState] = useState({
     persons: [
@@ -53,53 +123,4 @@ const app = props => {
 
 export default app;
 
-/*
-class App extends Component {
-  state = {
-    persons: [
-      { name: "Max", age: 28 },
-      { name: "Yishai", age: 30 },
-      { name: "Stephanie", age: 26 }
-    ]
-  };
-
-  addPersonHandler = () => {
-    console.log("addPerson was clicked");
-
-    this.state.persons[0].name = "react";
-    this.state.persons.push({ name: "new person", age: 100 });
-
-    console.table(this.state.persons);
-
-    this.setState({ persons: this.state.persons });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1>My first react app</h1>
-
-        <button onClick={this.addPersonHandler}>Add Person</button>
-
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <hr />
-        <h2> persons list</h2>
-        {this.state.persons.map(person => {
-          return <Person name={person.name} age={person.age} />;
-        })}
-
-        <hr />
-
-        <Person name="aaa" booksCount={4} bookCost={30} />
-        <Person name="person with children" booksCount={4} bookCost={30}>
-          i am a children !!!
-        </Person>
-      </div>
-    );
-  }
-}
-export default App;
 */
