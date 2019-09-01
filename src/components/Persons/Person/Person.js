c597d44c8532b18d23677f83dd7573019db10682
yishai <yishai5555@gmail.com>
2019-09-01 12:04:07 +0300

    b25

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./Person.css";
 
-const person = props => {
-  console.log(["Person.js rendering ...", props]);
-  return (
-    <div className="Person">
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  // const person = props => {
 
-export default person;
+  render() {
+    console.log(["Person.js rendering ...", this.props]);
+    return (
+      <div className="Person">
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
+}
+
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./Persons.css";
 import Person from "./Person/Person";
 
-const persons = props => {
-  console.log(["Persons.js rendering ...", props]);
-  return (
-    <div>
-      {props.persons.map((person, index) => {
-        return (
-          <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            key={index}
-            changed={event => props.changed(event, person.id)}
-          />
-        );
-      })}
-    </div>
-  );
-};
+class Persons extends Component {
+  // const persons = props => {
+  render() {
+    console.log(["Persons.js rendering ...", this.props]);
+    return (
+      <div>
+        {this.props.persons.map((person, index) => {
+          return (
+            <Person
+              click={() => this.props.clicked(index)}
+              name={person.name}
+              age={person.age}
+              key={index}
+              changed={event => this.props.changed(event, person.id)}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
-export default persons;
+export default Persons;
